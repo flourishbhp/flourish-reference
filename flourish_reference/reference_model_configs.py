b08f96352dc2818cd0ec44cea90c70c1f6784be6
tumiemosweu <imosweuk@gmail.com>
2021-02-05 17:23:16 +0200

    add refernce config

---
+++ flourish_reference/reference_model_configs.py
@@ -0,0 +1,13 @@
+from edc_reference import site_reference_configs
+
+site_reference_configs.register_from_visit_schedule(
+    visit_models={
+        'edc_appointment.appointment': ['flourish_caregiver.maternalvisit'],
+#         'edc_appointment.appointment': ['flourish_child.childvisit']
+        })
+
+configs = {}
+
+for reference_name, fields in configs.items():
+    site_reference_configs.add_fields_to_config(
+        name=reference_name, fields=fields)
