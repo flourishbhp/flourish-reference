2060285fa4949f77d85f6daea9bc32ddff062651
ame diphoko <diphokoame@gmail.com>
2021-04-08 12:46:27 +0200

    add depression screenings reference config

--- flourish_reference/reference_model_configs.py
+++ flourish_reference/reference_model_configs.py
@@ -6,7 +6,13 @@ site_reference_configs.register_from_visit_schedule(
         'flourish_child.appointment': ['flourish_child.childvisit'],
         })
 
-configs = {}
+configs = {
+    'flourish_caregiver.caregivergadanxietyscreening': ['anxiety_score'],
+    'flourish_caregiver.caregiverphqdeprscreening': ['depression_score',
+                                                     'self_harm'],
+    'flourish_caregiver.caregiveredinburghdeprscreening': ['depression_score',
+                                                           'self_harm']
+    }
 
 for reference_name, fields in configs.items():
     site_reference_configs.add_fields_to_config(
