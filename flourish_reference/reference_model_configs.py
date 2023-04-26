73bead80efab242232f09c2e14ab043843a91e0f
salahdin <salahdinjemal@gmail.com>
2023-04-26 10:23:22 +0200

    reverse deletion

--- flourish_reference/reference_model_configs.py
+++ flourish_reference/reference_model_configs.py
@@ -33,6 +33,12 @@ configs = {
     'flourish_caregiver.relationshipfatherinvolvement': ['partner_present'],
     'flourish_caregiver.tbengagement': ['interview_consent'],
     'flourish_caregiver.tbinterview': ['interview_language'],
+    'flourish_child.tbadolengagement': ['interview_consent'],
+    'flourish_child.tbadolinterview': ['interview_language'],
+    'flourish_child.tblabresultsadol': ['quantiferon_result'],
+    'flourish_child.tbvisitscreeningadolescent': ['cough_duration', 'fever_duration', 'night_sweats', 'weight_loss'],
+    'flourish_child.tbpresencehouseholdmembersadol': ['tb_referral', ],
+    'flourish_child.hivtestingadol': ['seen_by_healthcare', 'referred_for_treatment'],
 
 }
 
